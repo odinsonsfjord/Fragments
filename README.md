7558af3a98694903810ef4f81953aeb26db76dad
antonkazakov <antonanton08@gmail.com>
2022-10-18 22:46:11 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fragments
+
+Задание лежит здесь:
+https://kazakovanton.notion.site/Fragments-68897f1d94f143ec8db2ae9aa3e2aef6
--- app/src/main/java/otus/gpb/homework/fragments/ColorGenerator.kt
+++ app/src/main/java/otus/gpb/homework/fragments/ColorGenerator.kt
@@ -1,4 +1,13 @@
 package otus.gpb.homework.fragments
 
+import android.graphics.Color
+import java.util.Random
+
 object ColorGenerator {
+
+    fun generateColor(): Int {
+        val nextInt = Random().nextInt(0xffffff + 1)
+        val colorCode = String.format("#%06x", nextInt)
+        return Color.parseColor(colorCode)
+    }
 }
